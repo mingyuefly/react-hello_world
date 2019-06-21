659bad64b6521da7c910b01a3c1380d18402ba36
guomingyue <mingyuefly@qq.com>
2019-06-21 11:28:24 +0800

    JSX嵌入表达式

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import './index.css';
 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
 
-// JSX 标签
-//const element = <h1>Hello, world!</h1>
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
 
-const name = "Josh Perez";
-const element = <h1>hello, {name}</h1>;
+//为了便于阅读，我们会将 JSX 拆分为多行。同时，我们建议将内容包裹在括号中，虽然这样做不是强制要求的，但是这可以避免遇到自动插入分号陷阱。
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
 
 ReactDOM.render(
   element,
-  document.getElementById('root')
+  document.getElementById('root')   
 );
