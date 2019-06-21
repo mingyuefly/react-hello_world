11e60f2e5d69948570f2693b7455d2a1f0f8257b
guomingyue <mingyuefly@qq.com>
2019-06-21 11:04:40 +0800

    JSX标签和大括号

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 //import './index.css';
 
 
+// JSX 标签
+//const element = <h1>Hello, world!</h1>
+
+const name = "Josh Perez";
+const element = <h1>hello, {name}</h1>;
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById("root")
+  element,
+  document.getElementById('root')
 );
