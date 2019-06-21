1d17005993f334de8df070a556b7c6ca000485cf
guomingyue <mingyuefly@qq.com>
2019-06-21 18:48:17 +0800

    提取组件

--- index.js
+++ index.js
@@ -51,22 +51,84 @@ import { format } from 'url';
 // )
 
 // 提取组件
+// function formatDate(date){
+//   return date.toLocaleDateString();
+// }
+
+// function Comment(props){
+//   return (
+//     <div className = "Comment">
+//       <div className = "UserInfo">
+//         <img className = "Avatar"
+//          src = {props.author.avataUrl}
+//          alt = {props.author.name} 
+//         />
+//         <div className = "UserInfo-name">
+//           {props.author.name}
+//         </div>
+//       </div>
+//       <div className = "Comment-text">
+//         {props.text}
+//       </div>
+//       <div className = "Comment-date">
+//         {formatDate(props.date)}
+//       </div>
+//     </div>
+//   );
+// }
+
+// const comment = {
+//   date:new Date(),
+//   text:"I hope you enjoy learning react!",
+//   author:{
+//     name:"Hello Kitty",
+//     avataUrl:"https://placekitten.com/g/64/64"
+//   }
+// }
+
+// // ReactDOM.render(
+// //   Comment(comment),
+// //   document.getElementById('root')
+// // );
+
+// ReactDOM.render(
+//   <Comment 
+//     date = {comment.date}
+//     text = {comment.text}
+//     author = {comment.author}
+//   />,
+//   document.getElementById('root')
+// )
+
 function formatDate(date){
   return date.toLocaleDateString();
+}
+
+function Avatar(props){
+  return (
+    <img 
+      className = "Avatar"
+      src = {props.user.avataUrl}
+      alt = {props.user.name}
+    />
+  );
+}
+
+function UserInfo(props){
+  return (
+    <div className = "UserInfo">
+      <Avatar user = {props.user} />
+      <div className = "UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  )
 }
 
 function Comment(props){
   return (
     <div className = "Comment">
-      <div className = "UserInfo">
-        <img className = "Avatar"
-         src = {props.author.avataUrl}
-         alt = {props.author.name} 
-        />
-        <div className = "UserInfo-name">
-          {props.author.name}
-        </div>
-      </div>
+      <UserInfo user = {props.author} />
       <div className = "Comment-text">
         {props.text}
       </div>
@@ -99,6 +161,12 @@ ReactDOM.render(
   />,
   document.getElementById('root')
 )
+
+
+
+
+
+
 
 
 
