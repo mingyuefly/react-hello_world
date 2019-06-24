fa65e3603690f3ccf527b46b7deae70c2243179f
guomingyue <mingyuefly@qq.com>
2019-06-24 18:46:48 +0800

    添加state

--- index.js
+++ index.js
@@ -26,11 +26,17 @@ import { format } from 'url';
 // setInterval(tick, 1000);
 
 class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date:new Date(),
+    };
+  }
   render() {
     return (
       <div>
         <h1>Hello, world!</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
       </div>
     );
   }
@@ -38,7 +44,7 @@ class Clock extends React.Component {
 
 function tick() {
   ReactDOM.render(
-    <Clock date = {new Date()}/>,
+    <Clock />,
     document.getElementById('root')
   );
 }
