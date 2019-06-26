a8a7e8e60fcc9d9fb513838780c2242fc46bca5f
guomingyue <mingyuefly@qq.com>
2019-06-26 15:11:28 +0800

    元素变量

--- index.js
+++ index.js
@@ -21,7 +21,59 @@ function Greeting(props) {
   return <GuestGreeting />;
 }
 
+function LoginButton(props) {
+  return (
+  <button onClick = {props.onClick}>
+    Login
+  </button>);
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick = {props.onClick}>
+      Logout
+    </button>
+  )
+}
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn:false};
+  }
+
+  handleLoginClick() {
+    this.setState({
+      isLoggedIn:true
+    });
+  }
+
+  handleLogoutClick() {
+    this.setState({
+      isLoggedIn:false
+    });
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+    if (isLoggedIn) {
+      button = <LoginButton onClick = {this.handleLogoutClick} />;
+    } else {
+      button = <LogoutButton onClick = {this.handleLoginClick} />;
+    }
+    return (
+      <div>
+       <Greeting isLoggedIn = {this.state.isLoggedIn} />
+       {button}
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <Greeting isLoggedIn = {true} />,
+  <LoginControl />,
   document.getElementById('root')
 );
