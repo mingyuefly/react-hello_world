b67093eb5e185094e08fbb9e0d95c9619fbcdb16
guomingyue <mingyuefly@qq.com>
2019-06-26 14:48:43 +0800

    条件渲染

--- index.js
+++ index.js
@@ -4,40 +4,24 @@ import { format } from 'url';
 //import './index.css';
 
 
-//事件处理
+//条件渲染
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
+}
 
-class Toggle extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isToggleOn:true
-    }
-    // 为了在回调中使用 `this`，这个绑定是必不可少的
-    this.handclick = this.handclick.bind(this)
-  }
-  
-  // handclick() {
-  //   this.setState({
-  //     isToggleOn:!this.state.isToggleOn
-  //   });
-  // }
+function GuestGreeting(props) {
+  return <h1>Please sign up!</h1>;
+}
 
-  handclick() {
-    this.setState(state=>({
-      isToggleOn:!state.isToggleOn
-    }));
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
   }
-
-  render() {
-    return (
-    <button onClick = {this.handclick}>
-      {this.state.isToggleOn?'ON':'OFF'}
-    </button>
-    );
-  }
+  return <GuestGreeting />;
 }
 
 ReactDOM.render(
-  <Toggle />,
+  <Greeting isLoggedIn = {true} />,
   document.getElementById('root')
-)
+);
