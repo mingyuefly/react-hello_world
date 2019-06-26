6438ff52469e665f933627246d9e2db6bcd70ac5
guomingyue <mingyuefly@qq.com>
2019-06-26 15:29:59 +0800

    与运算符 &&

--- index.js
+++ index.js
@@ -5,75 +5,21 @@ import { format } from 'url';
 
 
 //条件渲染
-function UserGreeting(props) {
-  return <h1>Welcome back!</h1>;
-}
-
-function GuestGreeting(props) {
-  return <h1>Please sign up!</h1>;
-}
-
-function Greeting(props) {
-  const isLoggedIn = props.isLoggedIn;
-  if (isLoggedIn) {
-    return <UserGreeting />;
-  }
-  return <GuestGreeting />;
-}
-
-function LoginButton(props) {
+//与运算符 &&
+function Mailbox(props) {
+  const unreadMessages = props.unreadMessages;
   return (
-  <button onClick = {props.onClick}>
-    Login
-  </button>);
-}
-
-function LogoutButton(props) {
-  return (
-    <button onClick = {props.onClick}>
-      Logout
-    </button>
-  )
-}
-
-class LoginControl extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleLoginClick = this.handleLoginClick.bind(this);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
-    this.state = {isLoggedIn:false};
-  }
-
-  handleLoginClick() {
-    this.setState({
-      isLoggedIn:true
-    });
-  }
-
-  handleLogoutClick() {
-    this.setState({
-      isLoggedIn:false
-    });
-  }
-
-  render() {
-    const isLoggedIn = this.state.isLoggedIn;
-    let button;
-    if (isLoggedIn) {
-      button = <LoginButton onClick = {this.handleLogoutClick} />;
-    } else {
-      button = <LogoutButton onClick = {this.handleLoginClick} />;
+  <div>
+    <h1>Hello!</h1>
+    {
+      unreadMessages.length > 0 && <h2>You have {unreadMessages.length} unread messages.</h2>
     }
-    return (
-      <div>
-       <Greeting isLoggedIn = {this.state.isLoggedIn} />
-       {button}
-      </div>
-    )
-  }
+  </div>
+  );
 }
 
+const messages = ['React', 'Re: React', 'Re:Re: React'];
 ReactDOM.render(
-  <LoginControl />,
+  <Mailbox unreadMessages = {messages} />,
   document.getElementById('root')
-);
+)
