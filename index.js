57245573d4b0687896151ca7479a9991f9af38b9
guomingyue <mingyuefly@qq.com>
2019-06-27 16:46:05 +0800

    受控表单

--- index.js
+++ index.js
@@ -4,111 +4,38 @@ import ReactDOM from 'react-dom';
 //import './index.css';
 
 
-//类别&Key
+//表单
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value:''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-// const numbers = [1, 2, 3, 4, 5];
-// const doubled = numbers.map((number)=>number*2);
-// console.log(doubled)
+  handleChange(event) {
+    this.setState({value:event.target.value});
+  }
 
+  handleSubmit(event) {
+    alert('提交的名字'+this.state.value);
+    event.preventDefault();
+  }
 
-// const numbers = [1, 2, 3, 4, 5];
-// const listItems = numbers.map((number)=>
-//   <li>{number}</li>
-// )
-
-// function NumberList(props) {
-//   const numbers = props.numbers;
-//   const listItems = numbers.map((number)=>
-//     <li key = {number.toString()}>
-//       {number}
-//     </li>
-//   )
-//   return (<ul>{listItems}</ul>);
-// }
-
-// function ListItem(props) {
-//   return <li>{props.value}</li>;
-// }
-
-// function NumberList(props) {
-//   const numbers = props.numbers;
-//   const ListItems = numbers.map((number)=>
-//     <ListItem key = {number.toString()} value = {number} />
-//   );
-//   return (
-//     <ul>
-//       {ListItems}
-//     </ul>
-//   );
-// }
-
-// const numbers = [1, 2, 3, 4, 5];
-// ReactDOM.render(
-//   <NumberList numbers = {numbers} />,
-//   document.getElementById('root')
-// )
-
-
-//数组元素中使用的 key 在其兄弟节点之间应该是独一无二的。然而，它们不需要是全局唯一的。当我们生成两个不同的数组时，我们可以使用相同的 key 值
-// function Blog(props) {
-//   const sidebar = (
-//     <ul>
-//     {props.posts.map((post)=>
-//       <li key = {post.id}>
-//         {post.title}
-//       </li>
-//     )}
-//     </ul>
-//   );
-//   const content = (
-//     props.posts.map((post)=>
-//       <div key = {post.id}>
-//         <h3>{post.title}</h3>
-//         <p>{post.content}</p>
-//       </div>
-//     )
-//   );
-//   return (
-//     <div>
-//       {sidebar}
-//       <hr />
-//       {content}
-//     </div>
-//   );
-// }
-
-// const posts = [
-//   {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
-//   {id: 2, title: 'Installation', content: 'You can install React from npm.'}
-// ];
-
-// ReactDOM.render(
-//   <Blog posts = {posts} />,
-//   document.getElementById('root')
-// )
-
-
-// JSX中插入map()
-function ListItem(props) {
-  return <li>{props.value}</li>;
+  render() {
+    return (
+      <form onSubmit = {this.handleSubmit}>
+        <label>
+          名字：
+          <input type = "text" value = {this.state.value} onChange = {this.handleChange} />
+        </label>
+        <input type = "submit" value = "提交" />
+      </form>
+    );
+  }
 }
-
-function NumberList(props) {
-  const numbers = props.numbers;
-  return (
-    <ul>
-      {
-        numbers.map((number)=>
-          <ListItem key = {number.toString()} value = {number}></ListItem>
-        )
-      }
-    </ul>
-  );
-}
-
-const numbers = [1, 2, 3, 4, 5];
 
 ReactDOM.render(
-  <NumberList numbers = {numbers} />,
+  <NameForm />,
   document.getElementById('root')
-)
+);
