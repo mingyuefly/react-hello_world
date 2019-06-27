5ab31394b2ac382489186b6106559e0e517c3862
guomingyue <mingyuefly@qq.com>
2019-06-27 14:43:12 +0800

    数组元素中使用的 key 在其兄弟节点之间应该是独一无二的。然而，它们不需要是全局唯一的。当我们生成两个不同的数组时，我们可以使用相同的 key 值

--- index.js
+++ index.js
@@ -26,25 +26,62 @@ import ReactDOM from 'react-dom';
 //   return (<ul>{listItems}</ul>);
 // }
 
-function ListItem(props) {
-  return <li>{props.value}</li>;
-}
+// function ListItem(props) {
+//   return <li>{props.value}</li>;
+// }
 
-function NumberList(props) {
-  const numbers = props.numbers;
-  const ListItems = numbers.map((number)=>
-    <ListItem key = {number.toString()} value = {number} />
-  );
-  return (
+// function NumberList(props) {
+//   const numbers = props.numbers;
+//   const ListItems = numbers.map((number)=>
+//     <ListItem key = {number.toString()} value = {number} />
+//   );
+//   return (
+//     <ul>
+//       {ListItems}
+//     </ul>
+//   );
+// }
+
+// const numbers = [1, 2, 3, 4, 5];
+// ReactDOM.render(
+//   <NumberList numbers = {numbers} />,
+//   document.getElementById('root')
+// )
+
+function Blog(props) {
+  const sidebar = (
     <ul>
-      {ListItems}
+    {props.posts.map((post)=>
+      <li key = {post.id}>
+        {post.title}
+      </li>
+    )}
     </ul>
   );
+  const content = (
+    props.posts.map((post)=>
+      <div key = {post.id}>
+        <h3>{post.title}</h3>
+        <p>{post.content}</p>
+      </div>
+    )
+  );
+  return (
+    <div>
+      {sidebar}
+      <hr />
+      {content}
+    </div>
+  );
 }
 
-const numbers = [1, 2, 3, 4, 5];
+const posts = [
+  {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
+  {id: 2, title: 'Installation', content: 'You can install React from npm.'}
+];
+
 ReactDOM.render(
-  <NumberList numbers = {numbers} />,
+  <Blog posts = {posts} />,
   document.getElementById('root')
 )
 
