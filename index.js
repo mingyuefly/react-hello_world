a4948e42196510603564fa00d2fe0ab1c63514c2
guomingyue <mingyuefly@qq.com>
2019-06-27 11:36:02 +0800

    key的正确使用

--- index.js
+++ index.js
@@ -16,14 +16,30 @@ import ReactDOM from 'react-dom';
 //   <li>{number}</li>
 // )
 
+// function NumberList(props) {
+//   const numbers = props.numbers;
+//   const listItems = numbers.map((number)=>
+//     <li key = {number.toString()}>
+//       {number}
+//     </li>
+//   )
+//   return (<ul>{listItems}</ul>);
+// }
+
+function ListItem(props) {
+  return <li>{props.value}</li>;
+}
+
 function NumberList(props) {
   const numbers = props.numbers;
-  const listItems = numbers.map((number)=>
-    <li key = {number.toString()}>
-      {number}
-    </li>
-  )
-  return (<ul>{listItems}</ul>);
+  const ListItems = numbers.map((number)=>
+    <ListItem key = {number.toString()} value = {number} />
+  );
+  return (
+    <ul>
+      {ListItems}
+    </ul>
+  );
 }
 
 const numbers = [1, 2, 3, 4, 5];
@@ -31,4 +47,5 @@ ReactDOM.render(
   <NumberList numbers = {numbers} />,
   document.getElementById('root')
 )
+
 
