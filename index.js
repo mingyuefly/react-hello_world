09c652797a3a335acc2992222f0669526e4618bd
guomingyue <mingyuefly@qq.com>
2019-07-01 14:39:06 +0800

    组合 VS 继承 预留空间

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import { format } from 'url';
-//import './index.css';
+import { format } from 'url';
+import './index.css';
 
 
 //组合 VS 继承
-function FancyBorder(props) {
+//预留空间
+
+function Contacts() {
+  return <div className = "Contacts" />;
+}
+
+function Chat() {
+  return <div className = "Chat" />;
+}
+
+function SplitPane(props) {
   return (
-    <div className = {'FancyBorder FancyBorder-' + props.color}>
-      {props.children}
+    <div className = "SplitPane">
+      <div className = "SplitPane-left">
+        {props.left}
+      </div>
+      <div className = "SplitPane-right">
+        {props.right}
+      </div>
     </div>
   );
 }
 
-function WelcomeDialog() {
+function App() {
   return (
-    <FancyBorder color = "blue">
-      <h1 className = "Dialog-title">
-        Welcome
-      </h1>
-      <p className = "Dialog-message">
-        Thank you for visiting our spacecraft!
-      </p>
-    </FancyBorder>
+    <SplitPane left = {<Contacts />} right = {<Chat />} />
   );
 }
 
 ReactDOM.render(
-  <WelcomeDialog />,
+  <App />,
   document.getElementById('root')
 )
-
