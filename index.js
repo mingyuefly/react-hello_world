6300c4e2cdeb04bda0061cd1a2caf1608d80c719
guomingyue <mingyuefly@qq.com>
2019-07-01 16:19:03 +0800

    Class组合形式

--- index.js
+++ index.js
@@ -24,18 +24,42 @@ function Dialog(props) {
       <p className = "Dialog-message">
         {props.message}
       </p>
+      {props.children}
     </FancyBorder>
   );
 }
 
-function WelcomeDialog() {
-  return (
-    <Dialog title = "Welcome" message = "Thank you for visting our spacecrift" />
-  );
+class SignUpDialog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+    this.state = {login:''};
+  }
+
+  handleChange(e) {
+    this.setState({login:e.target.value});
+  }
+
+  handleSignUp(e) {
+    //alert('Welcome aboard, '+this.state.login);
+    //alert('Welcome aboard, ${this.state.login}!');
+    alert(`Welcome aboard, ${this.state.login}!`);
+  }
+
+  render() {
+    return (
+      <Dialog title = "Mars Exploration Program" message = "How should we refer to you?">
+        <input value = {this.state.login} onChange = {this.handleChange} />
+        <button onClick = {this.handleSignUp}>
+          Sign me up!
+        </button>
+      </Dialog>
+    );
+  }
 }
 
 ReactDOM.render(
-  <WelcomeDialog />,
+  <SignUpDialog />,
   document.getElementById('root')
 )
-
